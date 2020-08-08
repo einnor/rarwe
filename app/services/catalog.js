fbd85d69f0ec6f85074d93257178de6283ab7751
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-08 12:27:35 +0300

    Service method to add band and song record

--- app/services/catalog.js
+++ app/services/catalog.js
@@ -1,4 +1,17 @@
 import Service from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class CatalogService extends Service {
+  @tracked storage = {};
+
+  constructor() {
+    super(...arguments);
+    this.storage.bands = [];
+    this.storage.songs = [];
+  }
+
+  add(type, record) {
+    const collection = type === 'band' ? this.bands : this.songs;
+    collection.push(record);
+  }
 }
