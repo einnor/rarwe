cd34790b4048537108cad733ef27ad3027505577
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-08 12:57:25 +0300

    Update bands/new controller to add bands and songs via catalog service

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -2,10 +2,14 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { dasherize } from '@ember/string';
 import { Band } from 'rarwe/routes/bands';
+import { inject as service } from '@ember/service';
 
 export default class BandsNewController extends Controller {
+  @service catalog;
+
   @action
   saveBand() {
-    new Band({ name: this.name, slug: dasherize(this.name) })
+    const band = new Band({ name: this.name, slug: dasherize(this.name)});
+    this.catalog.add('band', band);
   }
 }
