0baf7fba1674633bbc18a39ca5b0961306441640
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-26 16:16:03 +0300

    Refactor to use the catalog service to fetch all bands

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,23 +1,10 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import Band from 'rarwe/models/band';
-import fetch from 'fetch';
 
 export default class BandsRoute extends Route {
   @service catalog;
 
-  async model() {
-    const response = await fetch('/bands');
-    const json = await response.json();
-    for (let item of json.data) {
-      const { id, attributes, relationships } = item;
-      const rels = {};
-      for (let relationshipName in relationships) {
-        rels[relationshipName] = relationships[relationshipName].links.related;
-      }
-      const record = new Band({ id, ...attributes }, rels);
-      this.catalog.add('band', record);
-    }
-    return this.catalog.bands;
+  model() {
+    return this.catalog.fetchAll();
   }
 }
