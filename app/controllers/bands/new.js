4de16205f7c46fcf94b102ca4d6cf1aef660dfc2
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-27 09:57:11 +0300

    Refactor creation of bands

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import Band from 'rarwe/models/band';
 import { inject as service } from '@ember/service';
-import fetch from 'fetch';
 
 export default class BandsNewController extends Controller {
   @service catalog;
@@ -10,24 +8,7 @@ export default class BandsNewController extends Controller {
 
   @action
   async saveBand() {
-    const response = await fetch('/bands', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/vnd.api+json'
-      },
-      body: JSON.stringify({
-        data: {
-          type: 'bands',
-          attributes: {
-            name: this.name
-          }
-        }
-      })
-    });
-    const json = await response.json();
-    const { id, attributes } = json.data;
-    const record = new Band({ id, ...attributes });
-    this.catalog.add('band', record);
-    this.router.transitionTo('bands.band.songs', id);
+    const band = await this.catalog.create('band', { name: this.name });
+    this.router.transitionTo('bands.band.songs', band.id);
   }
 }
