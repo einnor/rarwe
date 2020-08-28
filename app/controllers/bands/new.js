a086e48fbf453fc40a9af7c1a29432d003f11387
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-28 15:10:37 +0300

    routeWillChange hook to prompt the user for confirmation when leaving a page

--- app/controllers/bands/new.js
+++ app/controllers/bands/new.js
@@ -6,6 +6,20 @@ export default class BandsNewController extends Controller {
   @service catalog;
   @service router;
 
+  constructor() {
+    super(...arguments);
+    this.router.on('routeWillChange', transition => {
+      if (transition.from.name === 'bands.new') {
+        if (this.name) {
+          const leave = window.confirm('You have unsaved changes. Are you sure?');
+          if (!leave) {
+            transition.abort();
+          }
+        }
+      }
+    });
+  }
+
   @action
   async saveBand() {
     const band = await this.catalog.create('band', { name: this.name });
