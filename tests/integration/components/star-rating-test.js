3772d53be5ef0101f01f5c9f0ad64dcd178aaa25
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-03 13:52:55 +0300

    Star rating integration test - Renders the full and empty stars correctly

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -1,26 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | star-rating', function(hooks) {
   setupRenderingTest(hooks);
-
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<StarRating />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('Renders the full and empty stars correctly', async function(assert) {
+    assert.expect(4);
+    this.set('rating', 4);
+    this.set('updateRating', () => {});
     await render(hbs`
-      <StarRating>
-        template block text
-      </StarRating>
+      <StarRating
+        @rating={{this.rating}}
+        @onUpdate={{this.updateRating}}
+      />
     `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  assert.dom('[data-test-rr="full-star"]').exists({ count: 4 }, 'The right amount of full stars is rendered');
+  assert.dom('[data-test-rr="empty-star"]').exists({ count: 1 }, 'The right amount of empty stars is rendered');
+  this.set('rating', 2);
+  assert.dom('[data-test-rr="full-star"]').exists({ count: 2 }, 'The right amount of full stars is rendered after changing rating');
+  assert.dom('[data-test-rr="empty-star"]').exists({ count: 3 }, 'The right amount of empty stars is rendered after changing rating');
   });
 });
