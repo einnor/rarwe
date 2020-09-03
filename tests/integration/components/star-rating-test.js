938a26dbf0a3ecbbdabf542c32bf4495395192da
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-03 13:55:27 +0300

    Star rating integration test - Calls onUpdate with the correct value

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -21,4 +21,19 @@ module('Integration | Component | star-rating', function(hooks) {
   assert.dom('[data-test-rr="full-star"]').exists({ count: 2 }, 'The right amount of full stars is rendered after changing rating');
   assert.dom('[data-test-rr="empty-star"]').exists({ count: 3 }, 'The right amount of empty stars is rendered after changing rating');
   });
+
+  test('Calls onUpdate with the correct value', async function(assert) {
+    this.set('rating', 2);
+    this.set('updateRating', (rating) => {
+      assert.step(`Updated to rating: ${rating}`);
+    });
+    await render(hbs`
+      <StarRating
+        @rating={{this.rating}}
+        @onUpdate={{this.updateRating}}
+      />
+    `);
+    await click('[data-test-rr="star-rating-button"]:nth-child(4)');
+    assert.verifySteps(['Updated to rating: 4']);
+  });
 });
