3751fb2d420984734bdc84ffa04f9b33ccfc3250
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-03 11:11:34 +0300

    Installed ember-test-selectors

--- mirage/config.js
+++ mirage/config.js
@@ -25,4 +25,10 @@ export default function() {
   */
   this.get('/bands');
   this.get('/bands/:id');
+  this.get('/bands/:id/songs', function(schema, request) {
+    const id = request.params.id;
+    return schema.songs.where({ bandId: id });
+  });
+
+  this.post('/bands');
 }
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, click, fillIn } from '@ember/test-helpers';
+import { visit, click, fillIn, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
@@ -26,6 +26,7 @@ module('Acceptance | Bands', function(hooks) {
     await click('a[href="/bands/new"]');
     await fillIn('input', 'Caspian');
     await click('button');
+    await waitFor('p.text-center');
     const bandLinks = document.querySelectorAll('.mb-2 > a');
     assert.equal(bandLinks.length, 2, 'All band links are rendered', 'A new band link is rendered');
     assert.ok(bandLinks[1].textContent.includes('Caspian'), 'The new band link is rendered as the last item');
