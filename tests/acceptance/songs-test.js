8e7e8389b0253ec6692f5d1f95e778fcde71c537
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-04 14:59:06 +0300

    Added test to test sorting songs and buttons to order songs in desc and asc

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -18,5 +18,20 @@ module('Acceptance | songs', function(hooks) {
     await click('[data-test-rr=band-link]');
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Elephants', 'The first song is the one that comes first in the alphabet');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning in Daffodils', 'The last song is the one that comes last in the alphabet');
+
+    await click('[data-test-rr=sort-by-title-desc]');
+
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song is the one that comes last in the alphabet');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Elephants', 'The last song is the one that comes first in the alphabet');
+
+    await click('[data-test-rr=sort-by-rating-asc]');
+
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Mind Eraser, No Chaser', 'The first song is the lowest rated');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning in Daffodils', 'The last song is the highest rated');
+
+    await click('[data-test-rr=sort-by-rating-desc]');
+
+    assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song is the highest rated');
+    assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Mind Eraser, No Chaser', 'The last song is the lowest rated');
   });
 });
