3f1e0fd31df15df5a7b1e9f0cc51dbe2b9dbd14c
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-09-04 15:11:07 +0300

    Added tests asserting that the correct query params were showing in url

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -23,15 +23,18 @@ module('Acceptance | songs', function(hooks) {
 
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song is the one that comes last in the alphabet');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Elephants', 'The last song is the one that comes first in the alphabet');
+    assert.ok(currentURL().includes('s=-title'), 'The sort query param appears in the URL with the correct value');
 
     await click('[data-test-rr=sort-by-rating-asc]');
 
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Mind Eraser, No Chaser', 'The first song is the lowest rated');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Spinning in Daffodils', 'The last song is the highest rated');
+    assert.ok(currentURL().includes('s=rating'), 'The sort query param appears in the URL with the correct value');
 
     await click('[data-test-rr=sort-by-rating-desc]');
 
     assert.dom('[data-test-rr=song-list-item]:first-child').hasText('Spinning in Daffodils', 'The first song is the highest rated');
     assert.dom('[data-test-rr=song-list-item]:last-child').hasText('Mind Eraser, No Chaser', 'The last song is the lowest rated');
+    assert.ok(currentURL().includes('s=-rating'), 'The sort query param appears in the URL with the correct value');
   });
 });
