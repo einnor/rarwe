04c909a113fc9445913512b26d0b601fbe08dec0
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-12 09:14:44 +0300

    Loading placeholder for bands

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,10 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+function wait(delay) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
+  async model() {
+    await wait(3000);
     return this.catalog.fetchAll('bands');
   }
 }
